47e98975d1ac1864fe122766e546236e68227dd2
Muhammad Haseeb <mhaseeb@fiu.edu>
2019-08-23 19:16:35 -0400

    [PCDS-21] Bug fixing (accidental removal of memcpy)

--- cfirindex/src/utils.cpp
+++ cfirindex/src/utils.cpp
@@ -578,3 +578,13 @@ void UTILS_LinearRegression(INT n, DOUBLE x[], DOUBLE y[], DOUBLE &a, DOUBLE &b)
 
     return;
 }
+
+extern "C"	
+{	
+/* some systems do not have newest memcpy@@GLIBC_2.14 - stay with old good one */	
+asm (".symver memcpy, memcpy@GLIBC_2.2.5");	
+void *__wrap_memcpy(void *dest, const void *src, size_t n)	
+{	
+    return memcpy(dest, src, n);	
+}	
+}
