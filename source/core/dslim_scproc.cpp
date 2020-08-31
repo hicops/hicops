0a9b0d1c22ed2b3795b1f83745569463b269bb16
Muhammad Haseeb <mhaseeb@fiu.edu>
2020-08-31 19:29:42 -0400

    reverting barrier changes

--- source/core/dslim_scproc.cpp
+++ source/core/dslim_scproc.cpp
@@ -179,8 +179,7 @@ status_t DSLIM_DistScoreManager()
             sync_penalty.start();
 
             // wait for synchronization
-            //tim::mpi::barrier(MPI_COMM_WORLD);
-            status = MPI_Barrier(MPI_COMM_WORLD);
+            tim::mpi::barrier(MPI_COMM_WORLD);
 
             sync_penalty.stop();
 #else
