f9db831855b2009b7c0ce9a96d608738ce26f713
Muhammad Haseeb <mhaseeb@fiu.edu>
2020-09-04 17:54:32 -0400

    Adding wrapper for msconvert

---
+++ scripts/ms2prep/msconvert.sh
@@ -0,0 +1,40 @@
+#!@BASH_EXECUTABLE@
+# Convert RAW files to MS2 for HiCOPS on Linux
+# Copyrights(C) 2019 PCDS Laboratory
+# Muhammad Haseeb, and Fahad Saeed
+# School of Computing and Information Sciences
+# Florida International University (FIU), Miami, FL
+# Email: {mhaseeb, fsaeed}@fiu.edu
+#
+
+#
+# NOTE: Always run this script with sudo
+# USAGE: sudo msconvert /path/to/data*.RAW
+#
+
+# Set working directory from command line
+WDIR=$1
+
+# if not then use current directory
+if [ -z "$1" ]; then 
+    WDIR=$PWD
+fi
+
+# make a list of all the files that need processing
+ls -rt -d -1 "$WDIR"/{*,.*} > $WDIR/list
+
+# pull msconvert docker image
+docker pull chambm/pwiz-skyline-i-agree-to-the-vendor-licenses
+
+# run msconvert
+docker run -it --rm -v $WDIR:$WDIR chambm/pwiz-skyline-i-agree-to-the-vendor-licenses wine msconvert -f $WDIR/list --ms2 -o $WDIR/converted
+
+# make the output directory user accessible
+chown mhase003:user $WDIR/converted -R
+
+# wait for everything to sync up
+wait
+sync
+
+# remove the list
+rm -rf $WDIR/list
