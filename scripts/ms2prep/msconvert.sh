ee563fda06f281b61b9b5cac3f00e79fa363af31
Muhammad Haseeb <mhaseeb@fiu.edu>
2020-09-04 18:17:42 -0400

    updating username in msconvert

--- scripts/ms2prep/msconvert.sh
+++ scripts/ms2prep/msconvert.sh
@@ -32,7 +32,7 @@ docker pull chambm/pwiz-skyline-i-agree-to-the-vendor-licenses
 docker run -it --rm -v $WDIR:$WDIR chambm/pwiz-skyline-i-agree-to-the-vendor-licenses wine msconvert -f $WDIR/list --ms2 -o $WDIR/converted
 
 # make the output directory user accessible
-chown mhase003:user $WDIR/converted -R
+chown $USER:user $WDIR/converted -R
 
 # wait for everything to sync up
 wait
