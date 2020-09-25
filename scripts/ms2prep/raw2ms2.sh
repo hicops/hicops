40d719e6feb4e77e6be6d5c13a5f5f84e0b3c676
Muhammad Haseeb <mhase003@fiu.edu>
2020-09-25 19:50:49 -0400

    Update raw2ms2.sh

--- scripts/ms2prep/raw2ms2.sh
+++ scripts/ms2prep/raw2ms2.sh
@@ -33,7 +33,7 @@ docker pull chambm/pwiz-skyline-i-agree-to-the-vendor-licenses
 docker run -it --rm -v $WDIR:$WDIR chambm/pwiz-skyline-i-agree-to-the-vendor-licenses wine msconvert -f $WDIR/list --ms2 -o $WDIR/converted
 
 # make the output directory user accessible
-chown $USER:user $WDIR/converted -R
+chown ${SUDO_USER:-${USER}}:user $WDIR/converted -R
 
 # wait for everything to sync up
 wait
